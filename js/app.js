2b811fd69ae92d1784bab8edeb9dc9c6c318b28b
unknown <45887989+alematrix11@users.noreply.github.com>
2019-09-04 13:04:53 -0300

    Se comenzo a desarrollar la sesion del banner

---
+++ js/app.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let sidenav = document.querySelectorAll('.sidenav');
+    let instancia_sidenav = M.Sidenav.init(sidenav,{});
+    let dropdowns = document.querySelectorAll('.dropdown-trigger');
+    let instancia_dropwdown = M.Dropdown.init(dropdowns,{
+    hover:false});
+  });
+
+  $(document).ready(function(){
+
+    $('.carousel').carousel({fullWidth:true}).css("height", $(window).height());
+
+    // for next slide
+    $('.next').click(function(){
+      $('.carousel').carousel('next');
+    });
+
+      // for prev slide
+      $('.prev').click(function(){
+      $('.carousel').carousel('prev');
+    });
+
+    $('.carousel.carousel-slider').carousel({
+                fullWidth: true,
+                indicators: true
+              });
+
+  });
